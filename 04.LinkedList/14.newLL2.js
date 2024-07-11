dee446221c0565f8331d3af150c6bb1a77771305
Nikhil <ace.nikchauhan@gmail.com>
2024-07-11 13:31:03 +0530

    added new way of Linked list 2

---
+++ 04.LinkedList/14.newLL2.js
@@ -0,0 +1,84 @@
+class node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addAtHead(data) {
+    let newNode = new node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = newNode;
+      current.next = this.head;
+      this.head = current;
+    }
+  }
+
+  addAtTail(data) {
+    let newNode = new node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  removeAtHead() {
+    let current = this.head;
+    let next = current.next;
+    current.next = null;
+    this.head = next;
+  }
+
+  removeAtTail() {
+    let current = this.head;
+
+    while (current.next.next !== null) {
+      current = current.next;
+    }
+    current.next = null;
+  }
+
+  display() {
+    let current = this.head;
+    let result = "";
+    while (current !== null) {
+      result = result + current.data + " -> ";
+      current = current.next;
+    }
+    result = result + "NULL";
+    console.log(result);
+  }
+}
+
+const list = new LinkedList();
+
+list.addAtTail(4);
+list.addAtTail(8);
+list.addAtTail(65);
+
+list.display();
+
+list.addAtHead(88);
+list.addAtHead(56);
+
+list.display();
+
+list.removeAtHead();
+list.display();
+
+list.removeAtTail();
+list.display();
--- README.md
+++ README.md
@@ -66,5 +66,6 @@
     <li>11. Problem 1 using Recursion</li>
     <li>12. Problem 1 using Data Reversal Recursive</li>
     <li>13. Creating LL in a modern way</li>
+    <li>14. Creating LL in a modern way 2</li>
     <li></li>
 </ul>
